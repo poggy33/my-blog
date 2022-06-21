d8ab2c55848480bc659bc714d86f3f8263aded64
poggy33 <33poggy33@gmail.com>
2022-06-21 16:07:26 +0300

    change port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 const dbURI = "mongodb+srv://admin-ihor:33qwerty33@cluster0.ddemg.mongodb.net/node-tuts?retryWrites=true&w=majority"
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
+  .then(result => app.listen(process.env.PORT || 3000))
   .catch(err => console.log(err));
 
 // register view engine
